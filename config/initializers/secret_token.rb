5260d2dac53626f9a9b116c5d32b1988266ffdad
Hu Chen <huch0003@gmail.com>
2013-04-30 23:52:50 +0800

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SgProperty::Application.config.secret_token = 'f181051d35de812e4b74a3c1b905374b7f19bea9557700870ef5901ad8e12d5d8d1833e08895e24a98c8caf559f45b55b95a428d6405164c7a31d20fbfec851b'
---
+++ config/unicorn.rb
@@ -0,0 +1,35 @@
+# If you have a very small app you may be able to
+# increase this, but in general 3 workers seems to
+# work best
+worker_processes 3
+
+# Load your app into the master before forking
+# workers for super-fast worker spawn times
+preload_app true
+
+# Immediately restart any workers that
+# haven't responded within 30 seconds
+timeout 30
+
+before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  # per-process listener ports for debugging/admin/migrations
+  # addr = "127.0.0.1:#{9293 + worker.nr}"
+  # server.listen(addr, :tries => -1, :delay => 5, :tcp_nopush => true)
+
+  # the following is *required* for Rails + "preload_app true",
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+
+  # if preload_app is true, then you may also want to check and
+  # restart any other shared sockets/descriptors such as Memcached,
+  # and Redis.  TokyoCabinet file handles are safe to reuse
+  # between any number of forked children (assuming your kernel
+  # correctly implements pread()/pwrite() system calls)
+end
